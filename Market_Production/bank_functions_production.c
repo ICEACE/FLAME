d2f67ba7ee82214bdb0c7164a99a306503b2a356
bulentozel <bulent.ozel@gmail.com>
2013-04-11 16:16:01 +0300

    Adding skeleton of production market

---
+++ Market_Production/bank_functions_production.c
@@ -0,0 +1,14 @@
+#include "../header.h"
+#include "../bank_agent_header.h"
+
+/*
+ * \fn: int dummy function
+ * \brief:
+ */
+int bank_consumption_dummy()
+{
+
+
+	return 0; /* Returning zero means the agent is not removed */
+}
+
---
+++ Market_Production/firm_functions_production.c
@@ -0,0 +1,14 @@
+#include "../header.h"
+#include "../firm_agent_header.h"
+
+/*
+ * \fn: int dummy function
+ * \brief:
+ */
+int firm_consumption_dummy()
+{
+
+
+	return 0; /* Returning zero means the agent is not removed */
+}
+
