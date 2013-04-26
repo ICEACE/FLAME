4e6bf17aa249c5d8cf63c4c38cc9446dbc1236b8
bulentozel <bulent.ozel@gmail.com>
2013-04-26 17:31:24 +0300

    Design documents are updated, reflecting a newer implementation.

--- doxy/search/defines_62.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['bank_5fid',['BANK_ID',['../firm__agent__header_8h.html#a35c1d174c8e48ec46028cfc2ad690a7a',1,'BANK_ID():&#160;firm_agent_header.h'],['../household__agent__header_8h.html#a35c1d174c8e48ec46028cfc2ad690a7a',1,'BANK_ID():&#160;household_agent_header.h']]]
-];
