d7e19f5dcc234dd6b467b0b4bd5a6bdce612681c
bulentozel <bulent.ozel@gmail.com>
2013-04-26 17:19:48 +0300

    Model updates.

---
+++ equityfund_functions_top.c
@@ -0,0 +1,15 @@
+#include "header.h"
+#include "equityfund_agent_header.h"
+
+/*
+ * \fn: int equityfund_init()
+ * \brief: notifies its id by printing out agent id.
+ */
+int equityfund_init()
+{
+	// Read from memory
+	//printf("Equity Fund Agency = %d\n", ID);
+
+	return 0; /* Returning zero means the agent is not removed */
+}
+
