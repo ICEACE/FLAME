c4f76542e9919c2f74d78245e86306af41382ff3
bulentozel <bulent.ozel@gmail.com>
2013-05-05 19:04:38 +0300

    Addition of GPL terms and fictitious mall and job placement office agents.

---
+++ jpoffice_functions_top.c
@@ -0,0 +1,15 @@
+#include "header.h"
+#include "jpoffice_agent_header.h"
+
+/*
+ * \fn: int jpoffice_init()
+ * \brief: notifies its id by printing out agent id.
+ */
+int jpoffice_init()
+{
+	// Read from memory
+	//printf("Job Placement Agency = %d\n", ID);
+
+	return 0; /* Returning zero means the agent is not removed */
+}
+
---
+++ mall_functions_top.c
@@ -0,0 +1,15 @@
+#include "header.h"
+#include "mall_agent_header.h"
+
+/*
+ * \fn: int mall_init()
+ * \brief: notifies its id by printing out agent id.
+ */
+int mall_init()
+{
+	// Read from memory
+	//printf("Mall Agency = %d\n", ID);
+
+	return 0; /* Returning zero means the agent is not removed */
+}
+
