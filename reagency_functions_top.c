683f039689ee1a3a8e37a0f3787d3f0aaea99fbf
bulentozel <bulent.ozel@gmail.com>
2013-06-06 12:40:35 +0000

    Addition of real estate agency top function.

---
+++ reagency_functions_top.c
@@ -0,0 +1,15 @@
+#include "header.h"
+#include "reagency_agent_header.h"
+
+/*
+ * \fn: int reagency_init()
+ * \brief: notifies its id by printing out agent id.
+ */
+int reagency_init()
+{
+	// Read from memory
+	//printf("Real Estate Agency = %d\n", ID);
+
+	return 0; /* Returning zero means the agent is not removed */
+}
+
