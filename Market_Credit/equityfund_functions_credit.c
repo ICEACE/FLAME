0f62890030e9b436e4d220c433d3f0dc56f43916
bulentozel <bulent.ozel@gmail.com>
2013-06-22 09:29:35 +0000

    Update regarding Credit Market

---
+++ Market_Credit/equityfund_functions_credit.c
@@ -0,0 +1,70 @@
+#include "../header.h"
+#include "../equityfund_agent_header.h"
+
+/*
+ * \fn: equityfund_credit_invest_illiquids()
+ * \brief: Equity Fund does the balance sheet accounting.
+ */
+int equityfund_credit_invest_illiquids()
+{
+    
+	return 0; /* Returning zero means the agent is not removed */
+}
+
+
+/*
+ * \fn: int equityfund_credit_collect_firm_shares()
+ * \brief: Within this implementation the equity fund receives all of net income
+ * to be distributed equally to households.
+ */
+int equityfund_credit_collect_firm_shares()
+{
+    START_FIRM_NET_PROFIT_MESSAGE_LOOP
+    if (firm_net_profit_message->isconstructor == 1) {
+        SHARE_CONSTRUCTION_FIRMS += firm_net_profit_message->net_income;
+    } else {
+        SHARE_FIRMS += firm_net_profit_message->net_income;
+    }
+	FINISH_FIRM_NET_PROFIT_MESSAGE_LOOP
+    
+    // Net incomes are liquidified right away to be distributed to households.
+    LIQUIDITY = SHARE_CONSTRUCTION_FIRMS + SHARE_FIRMS;
+    SHARE_CONSTRUCTION_FIRMS = 0;
+    SHARE_FIRMS = 0;
+    
+	return 0; /* Returning zero means the agent is not removed */
+}
+
+
+/*
+ * \fn: int equityfund_credit_distribute_shares()
+ * \brief:Equity fund sends out shares to households.
+ */
+int equityfund_credit_distribute_shares()
+{
+    double per_house;
+    
+    per_house = LIQUIDITY / N_DIVIDENDS;
+    
+    add_household_share_message(per_house);
+    
+    LIQUIDITY = 0;
+    
+	return 0; /* Returning zero means the agent is not removed */
+}
+
+
+/*
+ * \fn: int equityfund_credit_do_balance_sheet()
+ * \brief: Equity Fund does the balance sheet accounting.
+ */
+int equityfund_credit_do_balance_sheet()
+{
+    EQUITY = LIQUIDITY + SHARE_FIRMS + SHARE_CONSTRUCTION_FIRMS + SHARE_BANKS;
+    
+     ////printf(" Equity Fund Id = %d, Equity %f \n", ID, EQUITY);
+    
+	return 0; /* Returning zero means the agent is not removed */
+}
+
+
---
+++ Market_Credit/government_functions_credit.c
@@ -0,0 +1,68 @@
+#include "../header.h"
+#include "../government_agent_header.h"
+
+
+
+/*
+ * \fn: int government_collect_capital_tax()
+ * \brief: 
+ */
+int government_collect_capital_tax()
+{
+    
+	return 0; /* Returning zero means the agent is not removed */
+}
+
+/*
+ * \fn: int government_update_fiscal_policy()
+ * \brief:
+ */
+int government_update_fiscal_policy()
+{
+    
+	return 0; /* Returning zero means the agent is not removed */
+}
+
+
+/*
+ * \fn: int government_do_balance_sheet()
+ * \brief: Government does the balance sheet accounting.
+ */
+int government_do_balance_sheet()
+{
+    EQUITY = LIQUIDITY - DEBT;
+    
+	return 0; /* Returning zero means the agent is not removed */
+}
+
+/*
+ * \fn: int government_collect_labour_tax()
+ * \brief:
+ */
+int government_collect_labour_tax()
+{
+    
+	return 0; /* Returning zero means the agent is not removed */
+}
+
+/*
+ * \fn: int government_distribute_general_benefits()
+ * \brief:
+ */
+int government_distribute_general_benefits()
+{
+    
+	return 0; /* Returning zero means the agent is not removed */
+}
+
+/*
+ * \fn: int government_distribute_unemployment_benefits()
+ * \brief:
+ */
+int government_distribute_unemployment_benefits()
+{
+    
+	return 0; /* Returning zero means the agent is not removed */
+}
+
+
