05e1106bcf33ff38eac2b8a6e438481ee5b09ae4
bulentozel <bulent.ozel@gmail.com>
2013-10-11 18:13:16 +0000

    Changes on initializations by PopGui.

--- compile_simulation_model.sh
+++ compile_simulation_model.sh
@@ -13,6 +13,8 @@
 
 # sequence of model compilation and simulation run
 cp ../initialization/instances/iceace_model_v1.0.0.xml ./initialization/iceace_model_v1.0.0.xml
+cp ../initialization/instances/iceace_model_v1.0.0.pop ./initialization/iceace_model_v1.0.0.pop
+
 cp ./initialization/iceace_model_v1.0.0.xml ./outputs/its/0.xml
 ../xparser-0.17.0/xparser model_iceace.xml
 cp ./stategraph_colour.dot ./docs/
