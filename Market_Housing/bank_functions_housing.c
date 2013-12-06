55aced9ec904e16e68564daa0885a6a0c5635386
bulentozel <bulent.ozel@gmail.com>
2013-12-06 15:43:19 +0000

    Bug fix at household deposits at Banks.

--- Market_Housing/bank_functions_housing.c
+++ Market_Housing/bank_functions_housing.c
@@ -79,7 +79,7 @@ int bank_housing_recieve_mortgages()
     MORTGAGES -= principal;
     LIQUIDITY += principal + interest;
     /* Households gets mortgages from their own bank only. */
-    DEPOSITS -= principal + interest;
+    //DEPOSITS -= principal + interest;
     INTERESTS_ACCRUED += interest;
     
 	FINISH_MORTGAGE_PAYMENT_MESSAGE_LOOP
