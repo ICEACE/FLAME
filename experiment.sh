f67d4272715fae107ac849864edb4e6bf09514c4
bulentozel <bulent.ozel@gmail.com>
2014-01-23 03:26:24 +0000

    Comparisons for beta = 0.2, 0.3. 0.4 and 0.5

--- experiment.sh
+++ experiment.sh
@@ -29,30 +29,30 @@ mv ./outputs/data/*.txt ./outputs/data/0.20/
 
 
 # Beta = 0.25 #############
-#sed 's|<household_budget_constraint>0.30</household_budget_constraint>|<household_budget_constraint>0.25</household_budget_constraint>|' ./outputs/its/iceace_model_v1.0.0.xml >  ./outputs/its/00.xml
-#sed 's|<collect_household_data>1</collect_household_data>|<collect_household_data>1</collect_household_data>|' ./outputs/its/00.xml >  ./outputs/its/0.xml
-#rm ./outputs/its/00.xml
+sed 's|<household_budget_constraint>0.30</household_budget_constraint>|<household_budget_constraint>0.25</household_budget_constraint>|' ./outputs/its/iceace_model_v1.0.0.xml >  ./outputs/its/00.xml
+sed 's|<collect_household_data>1</collect_household_data>|<collect_household_data>1</collect_household_data>|' ./outputs/its/00.xml >  ./outputs/its/0.xml
+rm ./outputs/its/00.xml
 
-#sed 's|betaval = "0.30"|betaval = "0.25"|' ./outputs/scripts/singleRunAnals.R > ./outputs/scripts/plot0.R
-#sed 's|niter = 3600|niter = 3600|' ./outputs/scripts/plot0.R > ./outputs/scripts/plot00.R
-#sed 's|data_household = 1|data_household = 1|' ./outputs/scripts/plot00.R > ./outputs/scripts/plot.R
-#rm ./outputs/scripts/plot0.R
-#rm ./outputs/scripts/plot00.R
+sed 's|betaval = "0.30"|betaval = "0.25"|' ./outputs/scripts/singleRunAnals.R > ./outputs/scripts/plot0.R
+sed 's|niter = 3600|niter = 3600|' ./outputs/scripts/plot0.R > ./outputs/scripts/plot00.R
+sed 's|data_household = 1|data_household = 1|' ./outputs/scripts/plot00.R > ./outputs/scripts/plot.R
+rm ./outputs/scripts/plot0.R
+rm ./outputs/scripts/plot00.R
 
-#./main 3600 ./outputs/its/0.xml -f 3600
+./main 3600 ./outputs/its/0.xml -f 3600
 
 
-#Rscript ./outputs/scripts/plot.R
-#rm ./outputs/plots/0.25/*.png
-#mv ./outputs/plots/*.png ./outputs/plots/0.25/
-#rm ./outputs/scripts/plot.R
+Rscript ./outputs/scripts/plot.R
+rm ./outputs/plots/0.25/*.png
+mv ./outputs/plots/*.png ./outputs/plots/0.25/
+rm ./outputs/scripts/plot.R
 
-#rm ./outputs/data/0.25/*.txt
-#mv ./outputs/data/*.txt ./outputs/data/0.25/
+rm ./outputs/data/0.25/*.txt
+mv ./outputs/data/*.txt ./outputs/data/0.25/
 
-#git add ./outputs/plots/0.25/*.png
-#git commit -a -m "Adding plots for beta = 0.25"
-#git push origin master
+git add ./outputs/plots/0.25/*.png
+git commit -a -m "Adding plots for beta = 0.25"
+git push origin master
 
 
 # Beta = 0.4 #############
@@ -136,38 +136,38 @@ mv ./outputs/data/*.txt ./outputs/data/0.50/
 
 
 # Beta = 0.55 #############
-#sed 's|<household_budget_constraint>0.30</household_budget_constraint>|<household_budget_constraint>0.55</household_budget_constraint>|' ./outputs/its/iceace_model_v1.0.0.xml >  ./outputs/its/00.xml
-#sed 's|<collect_household_data>1</collect_household_data>|<collect_household_data>1</collect_household_data>|' ./outputs/its/00.xml >  ./outputs/its/0.xml
-#rm ./outputs/its/00.xml
+sed 's|<household_budget_constraint>0.30</household_budget_constraint>|<household_budget_constraint>0.55</household_budget_constraint>|' ./outputs/its/iceace_model_v1.0.0.xml >  ./outputs/its/00.xml
+sed 's|<collect_household_data>1</collect_household_data>|<collect_household_data>1</collect_household_data>|' ./outputs/its/00.xml >  ./outputs/its/0.xml
+rm ./outputs/its/00.xml
 
-#sed 's|betaval = "0.30"|betaval = "0.55"|' ./outputs/scripts/singleRunAnals.R > ./outputs/scripts/plot0.R
-#sed 's|niter = 3600|niter = 3600|' ./outputs/scripts/plot0.R > ./outputs/scripts/plot00.R
-#sed 's|data_household = 1|data_household = 1|' ./outputs/scripts/plot00.R > ./outputs/scripts/plot.R
-#rm ./outputs/scripts/plot0.R
-#rm ./outputs/scripts/plot00.R
+sed 's|betaval = "0.30"|betaval = "0.55"|' ./outputs/scripts/singleRunAnals.R > ./outputs/scripts/plot0.R
+sed 's|niter = 3600|niter = 3600|' ./outputs/scripts/plot0.R > ./outputs/scripts/plot00.R
+sed 's|data_household = 1|data_household = 1|' ./outputs/scripts/plot00.R > ./outputs/scripts/plot.R
+rm ./outputs/scripts/plot0.R
+rm ./outputs/scripts/plot00.R
 
-#./main 3600 ./outputs/its/0.xml -f 3600
+./main 3600 ./outputs/its/0.xml -f 3600
 
 
-#Rscript ./outputs/scripts/plot.R
-#rm ./outputs/plots/0.55/*.png
-#mv ./outputs/plots/*.png ./outputs/plots/0.55/
-#rm ./outputs/scripts/plot.R
+Rscript ./outputs/scripts/plot.R
+rm ./outputs/plots/0.55/*.png
+mv ./outputs/plots/*.png ./outputs/plots/0.55/
+rm ./outputs/scripts/plot.R
 
-#rm ./outputs/data/0.55/*.txt
-#mv ./outputs/data/*.txt ./outputs/data/0.55/
+rm ./outputs/data/0.55/*.txt
+mv ./outputs/data/*.txt ./outputs/data/0.55/
 
-#git add ./outputs/plots/0.50/*.png
-#git commit -a -m "Adding plots for beta = 0.3"
-#git push origin master
+git add ./outputs/plots/0.55/*.png
+git commit -a -m "Adding plots for beta = 0.55"
+git push origin master
 
 # Overall Comparison Analysis ########
 
-#sed 's|data_household = 1|data_household = 1|' ./outputs/scripts/betaAnalaysis.R > ./outputs/scripts/plot.R
-#Rscript ./outputs/scripts/plot.R
-#rm ./outputs/scripts/plot.R
+sed 's|data_household = 1|data_household = 1|' ./outputs/scripts/betaAnalaysis.R > ./outputs/scripts/plot.R
+Rscript ./outputs/scripts/plot.R
+rm ./outputs/scripts/plot.R
 
-#git add ./outputs/plots/*.png
-#git commit -a -m "Adding overall plots on analyses of Beta effects"
-#git push origin master
+git add ./outputs/plots/*.png
+git commit -a -m "Adding overall plots on analyses of Beta effects"
+git push origin master
 
