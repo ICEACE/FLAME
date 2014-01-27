ef81ed90fb9eb9cd6f10aeddb36326141da57939
bulentozel <bulent.ozel@gmail.com>
2014-01-27 14:46:11 +0000

    The Makefile

--- experiment.sh
+++ experiment.sh
@@ -23,9 +23,9 @@ rm ./outputs/scripts/plot.R
 rm ./outputs/data/0.20/*.txt
 mv ./outputs/data/*.txt ./outputs/data/0.20/
 
-#git add ./outputs/plots/0.20/*.png
-#git commit -a -m "Adding plots for beta = 0.2"
-#git push origin master
+git add ./outputs/plots/0.20/*.png
+git commit -a -m "Adding plots for beta = 0.2"
+git push origin master
 
 
 # Beta = 0.25 #############
@@ -77,9 +77,9 @@ rm ./outputs/scripts/plot.R
 rm ./outputs/data/0.40/*.txt
 mv ./outputs/data/*.txt ./outputs/data/0.40/
 
-#git add ./outputs/plots/0.40/*.png
-#git commit -a -m "Adding plots for beta = 0.4"
-#git push origin master
+git add ./outputs/plots/0.40/*.png
+git commit -a -m "Adding plots for beta = 0.4"
+git push origin master
 
 
 # Beta = 0.3 #############
@@ -104,62 +104,11 @@ rm ./outputs/scripts/plot.R
 rm ./outputs/data/0.30/*.txt
 mv ./outputs/data/*.txt ./outputs/data/0.30/
 
-#git add ./outputs/plots/0.30/*.png
-#git commit -a -m "Adding plots for beta = 0.3"
-#git push origin master
-
-# Beta = 0.5 #############
-sed 's|<household_budget_constraint>0.30</household_budget_constraint>|<household_budget_constraint>0.50</household_budget_constraint>|' ./outputs/its/iceace_model_v1.0.0.xml >  ./outputs/its/00.xml
-sed 's|<collect_household_data>1</collect_household_data>|<collect_household_data>1</collect_household_data>|' ./outputs/its/00.xml >  ./outputs/its/0.xml
-rm ./outputs/its/00.xml
-
-sed 's|betaval = "0.30"|betaval = "0.50"|' ./outputs/scripts/singleRunAnals.R > ./outputs/scripts/plot0.R
-sed 's|niter = 3600|niter = 3600|' ./outputs/scripts/plot0.R > ./outputs/scripts/plot00.R
-sed 's|data_household = 1|data_household = 1|' ./outputs/scripts/plot00.R > ./outputs/scripts/plot.R
-rm ./outputs/scripts/plot0.R
-rm ./outputs/scripts/plot00.R
-
-./main 3600 ./outputs/its/0.xml -f 3600
-
-
-Rscript ./outputs/scripts/plot.R
-rm ./outputs/plots/0.50/*.png
-mv ./outputs/plots/*.png ./outputs/plots/0.50/
-rm ./outputs/scripts/plot.R
-
-rm ./outputs/data/0.50/*.txt
-mv ./outputs/data/*.txt ./outputs/data/0.50/
-
-#git add ./outputs/plots/0.50/*.png
-#git commit -a -m "Adding plots for beta = 0.3"
-#git push origin master
-
-
-# Beta = 0.55 #############
-sed 's|<household_budget_constraint>0.30</household_budget_constraint>|<household_budget_constraint>0.55</household_budget_constraint>|' ./outputs/its/iceace_model_v1.0.0.xml >  ./outputs/its/00.xml
-sed 's|<collect_household_data>1</collect_household_data>|<collect_household_data>1</collect_household_data>|' ./outputs/its/00.xml >  ./outputs/its/0.xml
-rm ./outputs/its/00.xml
-
-sed 's|betaval = "0.30"|betaval = "0.55"|' ./outputs/scripts/singleRunAnals.R > ./outputs/scripts/plot0.R
-sed 's|niter = 3600|niter = 3600|' ./outputs/scripts/plot0.R > ./outputs/scripts/plot00.R
-sed 's|data_household = 1|data_household = 1|' ./outputs/scripts/plot00.R > ./outputs/scripts/plot.R
-rm ./outputs/scripts/plot0.R
-rm ./outputs/scripts/plot00.R
-
-./main 3600 ./outputs/its/0.xml -f 3600
-
-
-Rscript ./outputs/scripts/plot.R
-rm ./outputs/plots/0.55/*.png
-mv ./outputs/plots/*.png ./outputs/plots/0.55/
-rm ./outputs/scripts/plot.R
-
-rm ./outputs/data/0.55/*.txt
-mv ./outputs/data/*.txt ./outputs/data/0.55/
-
-git add ./outputs/plots/0.55/*.png
-git commit -a -m "Adding plots for beta = 0.55"
+git add ./outputs/plots/0.30/*.png
+git commit -a -m "Adding plots for beta = 0.3"
 git push origin master
+
+
 
 # Overall Comparison Analysis ########
 
