dcb8df7b044dc83d85397ae668950099734dd971
bulentozel <bulent.ozel@gmail.com>
2014-01-28 21:27:52 +0000

    Adding foreign sector top functions.

---
+++ foreignsector_functions_top.c
@@ -0,0 +1,22 @@
+#include "header.h"
+#include "centralbank_agent_header.h"
+
+
+
+
+/*
+ * \fn: int centralbank_iterate()
+ * \brief: 
+ */
+int centralbank_iterate()
+{
+    if (DATA_COLLECTION_MODE) {
+        
+    }
+    
+    IT_NO++;
+	return 0; /* Returning zero means the agent is not removed */
+}
+
+
+
