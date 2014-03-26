a7c7e8f61964ec71ec676de9582f5e7b32b77284
Einar Jón Erlingsson <einare09@ru.is>
2014-03-26 15:32:39 +0000

    Bad bank dummy and top function added
    added dummy function for bad bank in credit market and a simple top
    function as well.

---
+++ Market_Credit/badbank_functions_credit.c
@@ -0,0 +1,13 @@
+#include "../header.h"
+#include "../badbank_agent_header.h"
+
+/*
+ * \fn: int dummy function
+ * \brief:
+ */
+int badbank_credit_dummy()
+{
+
+
+	return 0; /* Returning zero means the agent is not removed */
+}
